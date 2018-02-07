048b673cda7b7637d50878ed2e2f6f51d3533815
mmp <mpinheiro@ophiomics.com>
2018-02-07 11:17:45 +0000

    [add] tests

---
+++ day_3/test_clean_fasta.py
@@ -0,0 +1,29 @@
+'''
+Created on Feb 7, 2018
+
+@author: mmp
+'''
+import unittest
+import second_module
+
+class Test(unittest.TestCase):
+
+
+	def test_clean_fasta(self):
+		self.assertEqual('AAACCTTT', second_module.clean_fasta('aaaccttt'))
+		self.assertEqual('AAACCTTT', second_module.clean_fasta('AAACCTTT'))
+		self.assertEqual('AAACCTTT', second_module.clean_fasta('asdsaaccttt'))
+		self.assertEqual('', second_module.clean_fasta(''))
+		self.assertEqual('AATTTUUUCCTT', second_module.clean_fasta('aatttuuucctt'))
+		self.assertTrue(second_module.clean_fasta('') == '')
+
+	def test_complement(self):
+		self.assertEqual('AATT', second_module.complement('TTAA'))
+		self.assertEqual('AATTGGCCDAA', second_module.complement('TTAACCGGDUu'))
+
+	def test_reserve(self):
+		self.assertEqual('AATT', second_module.reverse('TTAA'))
+
+if __name__ == "__main__":
+	#import sys;sys.argv = ['', 'Test.testName']
+	unittest.main()
